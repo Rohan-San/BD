b467f9609a752389b8f221ddc43a14d526c54698
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-04-08 10:44:32 +0530

    Create reducer.java

---
+++ OddEven/reducer.java
@@ -0,0 +1,17 @@
+package oddeven; 
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+
+public class reducer extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+  public void reduce(Text key, Iterator<IntWritable> value, OutputCollector<Text, IntWritable> output, Reporter r) throws IOException {
+    int sum=0, count=0;
+    while(value.hasNext()) {
+      sum+=value.next().get();
+      count++;
+    }
+    output.collect(new Text("Sum of "+key+" numbers"),new IntWritable(sum));
+    output.collect(new Text(key+" NUmber count"),new IntWritable(count));
+  }
+}
