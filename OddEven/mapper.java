2e3889222a3ae2ed0b693954e94df15ea50b363e
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-04-08 13:00:41 +0530

    Update mapper.java

--- OddEven/mapper.java
+++ OddEven/mapper.java
@@ -18,3 +18,25 @@ public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, 
     }
   } 
 }
+
+// Typed out_
+// package oddeven;
+// import java.io.*;
+// import java.util.*;
+// import org.apache.hadoop.mapred.*;
+// import org.apache.hadoop.io.*;
+
+// public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+//         public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter r) throws IOException {
+//                 String[] line = value.toString().split("");
+//                 for(String num:line) {
+//                         int number = Integer.parseInt(num);
+//                         if(number%2==0) {
+//                                 output.collect(new Text("even"), new IntWritable(number));
+//                         }
+//                         else {
+//                                 output.collect(new Text("odd"), new IntWritable(number));
+//                         }
+//                 }
+//         }
+// }
