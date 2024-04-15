12ec8522e4858c2aaa660db51fb03f6b0676f956
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-04-15 12:49:50 +0530

    Create mappercsv.java

---
+++ Weather/mappercsv.java
@@ -0,0 +1,21 @@
+package weather;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+
+public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
+        public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter r) throws IOException {
+                String line = value.toString();
+                String[] parts = line.split(",");
+                if (parts.length >= 2) {
+                        String year = parts[0].trim();
+                        try {
+                                Double temp = Double.parseDouble(parts[3].trim());
+                                output.collect(new Text(year), new DoubleWritable(temp));
+                        } catch (NumberFormatException e) {
+                                output.collect(new Text("Error"));
+                        }
+                }
+        }       
+} 
