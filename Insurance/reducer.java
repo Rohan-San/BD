a553eb62491a1b9e88211480525b69394017198d
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-06 12:48:28 +0530

    Create reducer.java

---
+++ Insurance/reducer.java
@@ -0,0 +1,17 @@
+package insurance;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+public class reducer extends MapReduceBase implements Reducer<Text,IntWritable,Text,IntWritable>
+{
+    public void reduce(Text key,Iterator<IntWritable> value,OutputCollector<Text,IntWritable> output ,Reporter r) throws IOException
+    {
+         int sum=0;
+         while(value.hasNext())
+         {
+                 sum+=value.next().get();
+         }
+         output.collect(key,new IntWritable(sum));
+   }
+}
