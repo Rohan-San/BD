a9ee5a3c5ba12b2e95379b397259ae8c74ea9e25
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-06 10:38:07 +0530

    Create mapper.java

---
+++ Employee/mapper.java
@@ -0,0 +1,13 @@
+package employee;
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+
+class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable> {
+        public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter r) throws IOException {
+                String[] line = value.toString().split("\\t");
+                salary = Double.parseDouble(line[8]);
+                output.collect(new Text(line[3]), new DoubleWritable(salary));
+        }
+}
