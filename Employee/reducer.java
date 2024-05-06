10089ad327f73b0344dc333fbb93f8e81c895a77
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-06 10:53:56 +0530

    Update reducer.java

--- Employee/reducer.java
+++ Employee/reducer.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.mapred.*;
 import org.apache.hadoop.io.*;
 
 class reducer extends MapReduceBase implements Reducer<Text, DoubleWritable, Text, DoubleWritable> {
-        public void reduce(Text key, Iterator<DoubleWritable> value, OutputCollector<Text, DoubleWritable> Reporter r) throws IOException {
+        public void reduce(Text key, Iterator<DoubleWritable> value, OutputCollector<Text, DoubleWritable> output, Reporter r) throws IOException {
                 int count = 0;
                 Double sum = 0.0;
                 while(value.hasNext()) {
