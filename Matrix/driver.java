d3e943cb9a2028fefce51f1e87d694f05a5075e6
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-13 10:30:14 +0530

    Create driver.java

---
+++ Matrix/driver.java
@@ -0,0 +1,19 @@
+package matrix;
+import java.util.*;
+import java.io.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.io.*;
+
+public class driver{
+    public static void main(String args[]) throws IOException {
+        JobConf conf=new JobConf(driver.class);
+        conf.setMapperClass(mapper.class);
+        conf.setReducerClass(reducer.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(conf,new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf,new Path(args[1]));
+        JobClient.runJob(conf);
+    }
+}
