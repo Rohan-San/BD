ad84b7cc39ab1ec6b14a6b287c5be85d111c8a7e
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-27 11:34:45 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,11 @@
+# Hadoop
 export JAVA_HOME=$(readlink -f $(which javac) | awk 'BEGIN {FS="/bin"} {print $1}')
 export PATH=$(echo $PATH):$(pwd)/bin
 export CLASSPATH=$(hadoop classpath)
+
+# Spark
+export JAVA_HOME=$(readlink -f $(which javac) | awk 'BEGIN {FS="/bin"} {print $1}')
+if ! command -v spark-shell --version &> /dev/null
+then
+    export PATH=$(echo $PATH):$(pwd)/bin
+fi
