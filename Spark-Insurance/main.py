5dfec8d5bb9b3b6123fab36514f143c9e6683de4
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-27 11:48:42 +0530

    Create main.py

---
+++ Spark-Insurance/main.py
@@ -0,0 +1,13 @@
+import sys
+if(len(sys.argv)!=3):
+    print("Provide input file and output directory")
+    sys.exit(0)
+  
+from pyspark import SparkContext
+sc = SparkContext()
+f = sc.textFile(sys.argv[1])
+
+temp = f.map(lambda x: (x.split(',')[16],1))
+data = temp.countByKey()
+dd = sc.parallelize(data.items())
+dd.saveAsTextFile(sys.argv[2])
