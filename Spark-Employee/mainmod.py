e88c4ba71c0118bdd89682d963da7e21aeba162e
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2024-05-27 12:04:57 +0530

    Create mainmod.py

---
+++ Spark-Employee/mainmod.py
@@ -0,0 +1,14 @@
+import sys
+if(len(sys.argv)!=3):
+    print("Provide Input File and Output Directory")
+    sys.exit(0)
+
+from pyspark import SparkContext
+sc =SparkContext()
+f = sc.textFile(sys.argv[1])
+
+temp=f.map(lambda x: (x.split('\t')[3],float(1)))
+
+total=temp.reduceByKey(lambda a,b : a+b)
+
+total.saveAsTextFile(sys.argv[2])
